891827f2d6b7f7ba0e2b4756723f6009434de699
nicosantone <nicolas.santone@davinci.edu.ar>
2025-04-24 16:30:31 -0300

    Gerente

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
-
+import javax.swing.JOptionPane;
+import java.util.LinkedList;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		LinkedList<Libro> libros = new LinkedList<>();
+		Gerente gerente = new Gerente("Lucas", "Martínez", "lucas@gmail.com", "1234", "12345678", "Ventas", 250000);
+		
+        gerente.registrarLibros(libros);
+        gerente.eliminarLibrosSinStock(libros);
+        gerente.mostrarLibrosDisponibles(libros);
+		
 	}
 
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -55,4 +55,6 @@ public class Usuario {
 		this.dni = dni;
 	}
 	
+
+	
 }
