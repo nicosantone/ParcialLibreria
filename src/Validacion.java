befadf2b9e32cc5d4369b162fd2511becbaf633b
facundoDVinci <facundo.husto@davinci.edu.ar>
2025-04-28 16:33:24 -0300

    Actualizacion del Main hecha por los 2

--- src/Validacion.java
+++ src/Validacion.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public interface Validacion {
 default boolean ValidarEmail(String email) {
 		
-		if(email.contains("@gmail.com")) {
+		if(email.contains("@libreria.com")) {
 			return true;
 		}else {
 			return false;
